0f36b93487055ea183c2227536a762d9566b941a
Matteo Caruso <m.caruso425@gmail.com>
2018-02-15 10:17:48 +0100

    testing

--- run.py
+++ run.py
@@ -30,6 +30,34 @@ def run_cmd(cmdstr):
     return (exit_code, lines_output, lines_errors)
 
 
+@app.route("/")
+def hello():
+    proc_exit, proc_out, proc_err = run_cmd('qstat -a')
+    if len(proc_out) < 1:
+        return build_resp([])
+    # Job ID  Username    Queue    Jobname          SessID  NDS   TSK   Memory   Time    S   Time
+
+    proc_out = proc_out[4:]
+    jobs = []
+    for line in proc_out:
+        linarr = line.split()
+        print linarr
+        jobs.append({
+            'job_id': linarr[0],
+            'user': linarr[1],
+            'queue': linarr[2],
+            'job': linarr[3],
+            'session': linarr[4],
+            'NDS': linarr[5],
+            'TSK': linarr[6],
+            'memory': linarr[7],
+            'reqTime': linarr[8],
+            'status': linarr[9],
+            'eleTime': linarr[10]
+        })
+    return build_resp(jobs)
+
+
 @app.route("/jobs")
 def hello():
     proc_exit, proc_out, proc_err = run_cmd('qstat -a')
